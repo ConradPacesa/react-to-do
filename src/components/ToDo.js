0e26d56c3495ce279f177714ded57ef754ba74e5
Conrad Pacesa <conradpacesa@gmail.com>
2017-12-27 14:59:23 -0500

    Added check functionality, add new todo

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,7 @@ class ToDo extends Component {
   render() {
     return (
       <li>
-        <input type="Checkbox" checked={ this.props.isCompleted } />
+        <input type="Checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
         <span>{ this.props.description }</span>
       </li>
     );
